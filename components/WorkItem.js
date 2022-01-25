d0eacc6e05455dd143f781f3afbf6ca4b4555cb2
Michael Beaujean <mbeaujean88@gmail.com>
2022-01-25 11:05:09 -0500

    Fix hover state

--- components/WorkItem.js
+++ components/WorkItem.js
@@ -1,8 +1,10 @@
+import { useState, useCallback } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import ConditionalWrapper from '../components/ConditionalWrapper';
 
 const WorkItem = (props) => {
+	const [hover, setHover] = useState(false);
 	const { project } = props;
 
 	const {
@@ -17,23 +19,9 @@ const WorkItem = (props) => {
 		duration: .4,
 	};
 
-	const bgVariants = {
-		hoverOff: {
-			width: 0,
-		},
-		hoverOn: {
-			width: '100%',
-		},
-	};
-
-	const stackContainerVariants = {
-		hoverOff: {
-			width: 0,
-		},
-		hoverOn: {
-			width: 'auto',
-		},
-	};
+	const onHover = useCallback(() => {
+		setHover(hover => !hover);
+	}, [setHover]);
 
 	return (
 		<li className="relative">
@@ -41,34 +29,33 @@ const WorkItem = (props) => {
 				condition={!link}
 				wrapper={children => <Link href={`/work/${slug}`}>{children}</Link>}
 			>
-				<motion.a
+				<a
 					{...link && {
 						href: link,
 						target: '_blank',
 						rel: 'noopener'
 					}}
-						whileHover="hoverOn"
-						className="inline-block pr-2 relative uppercase tracking-tight font-bold antialiased cursor-pointer"
-					>
+					onMouseEnter={onHover}
+					onMouseLeave={onHover}
+					className="inline-block pr-2 relative uppercase tracking-tight font-bold antialiased cursor-pointer"
+				>
 					<motion.div
 						className="absolute top-0 left-0 bg-mint w-full h-full"
-						initial="hoverOff"
-						variants={bgVariants}
+						animate={{ width: hover ? '100%' : 0 }}
 						transition={transition}
 					/>
 					<motion.div
 						className="absolute top-1/2 left-2 transform -translate-y-1/2 flex items-center h-1/2 z-3 overflow-hidden"
-						initial="hoverOff"
-						variants={stackContainerVariants}
+						animate={{ width: hover ? 'auto' : 0 }}
 						transition={transition}
 					>
-						<div className="absolute bg-black w-full h-full px-2"></div>
+						<div className="absolute bg-black w-full h-full px-2" />
 						<motion.span className="relative px-2 z-3 normal-case font-normal text-15px text-white whitespace-nowrap">
 							{stack}
 						</motion.span>
 					</motion.div>
 					<span className="text-24px lg:text-36px relative z-2">{title}</span>
-				</motion.a>
+				</a>
 			</ConditionalWrapper>
 		</li>
 	)
